9ea8d5e55fc4224a14886ee570a72c1ea6f78a07
mgillmann <gillmann.matt@gmail.com>
2015-08-26 20:04:18 -0500

    Players + Sprites
    Created player entities and sprites to go with selected starter pokemon.
    Need to update player if they get pikachu, menu functionality and
    options updated a bit

--- js/craftygame.js
+++ js/craftygame.js
@@ -1,12 +1,18 @@
 // OPTIONS
+// let's start with 4 players for simplicity sake
 var gameOptions = {
 	"MAX_PLAYERS": 4
 };
 
+var playersArray = [];
+
+var requiredSquares = [];
+
 Game = {
 	// Initialize and start our game
 	createMenu: function() {
-		Crafty.defineScene("menu", function(){
+		Crafty.scene("menu", function(){
+			$("#menu").show();
 			// TODO: take player # input
 			var select = document.getElementById('playerNum');
 
@@ -14,10 +20,10 @@ Game = {
 			
 			// Create the select
 			for(var i = 0; i<gameOptions.MAX_PLAYERS; i++) {
-				select.options[select.options.length] = new Option(i+1, i);
+				select.options[select.options.length] = new Option(i+1, i+1);
 			}
 			$("#maxPlayers").html("Max Players: " + gameOptions.MAX_PLAYERS);
-			generateMenu(gameOptions.MAX_PLAYERS);
+			
 			// TODO
 			// player name entry
 			// player character select: dropdown
@@ -28,30 +34,38 @@ Game = {
 
 	start: function() {
 		Game.createMenu();
-		Crafty.init(2216, 2216, document.getElementById('game'));
-		//Crafty.load(["./wall-old.png"])
-		Crafty.background("url('./wall-old.png')");
-		Crafty.canvas.init();
+		Crafty.init(2216, 2216, document.getElementById('game')).canvas.init();
 		// Game pieces for the 4 pokemon you can potentially have
-		// Need to create background colors per user for these to sit on
-		Crafty.sprite("./gamepieces.jpg", {bulbasaur:[7,7,50,50]});
-		Crafty.sprite("./gamepieces.jpg", {charmander:[398,10,50,50]});
-		Crafty.sprite("./gamepieces.jpg", {squirtle:[135,75,50,50]});
-		Crafty.sprite("./gamepieces.jpg", {pikachu:[523,265,50,50]});
-
+		// Need to create background colors per user for these to sit on (MAYBE?)
+		Crafty.sprite("./gamepieces_alpha.png", {
+											bulbasaur:[7,7,50,50],
+											charmander:[398,10,50,50],
+											squirtle:[135,75,50,50],
+											pikachu:[523,265,50,50]
+										});
 		Crafty.enterScene("menu");
-
-		// Displays the sprites
-		// var bulbasaur = Crafty.e("2D, DOM, bulbasaur");
-		// var charmander = Crafty.e("2D, DOM, charmander");
-		// var squirtle = Crafty.e("2D, DOM, squirtle");
-		// var pikachu = Crafty.e("2D, DOM, pikachu");	
 	},
 
-	initPlayers: function() {
-		//TODO create all players
-	}
+	playGame: function() {
+		Crafty.enterScene("game");
+	},
 
+	initPlayers: function(numPlayers, players) {
+		//TODO figure out how to resize image and zoom in on players
+		Crafty.scene("game", function() {
+			Crafty.background("url('./wall-old.png')");
+			$("#menu").hide();
+			for(i = 0;i < numPlayers;i++) {
+				// Create players, move to Pallet Town
+				playersArray[i] = Crafty.e("2D, DOM,"+players[i].starter)
+										.attr({x:325, y:1800});
+				playersArray[i].name = players[i].name;
+				//playersArray[i].starter = players[i].starter;
+			}
+		});
+		Game.playGame();
+		// Play the game after the players are created
+	}
 };
 
 // Select number of players

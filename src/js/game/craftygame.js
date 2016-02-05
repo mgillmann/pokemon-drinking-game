8066fd19b78054681ec21feac9cca4acee2bc436
Matt Zeman <zeman.mATT@GMAIL.COM>
2016-02-05 15:59:20 -0600

    Moving files into new folder structure

---
+++ src/js/game/craftygame.js
@@ -0,0 +1,140 @@
+// OPTIONS
+// let's start with 4 players for simplicity sake
+// debug option to display debug values on the front end (potential option)
+var gameOptions = {
+	"MAX_PLAYERS": 4,
+	"DEBUG" : false
+};
+
+var playersArray = [];
+
+// Start the game with the 1st player
+var currentPlayer = 0;
+
+// The number of players for the current game
+var currentNumPlayers = 0;
+
+Game = {
+	// Initialize and start our game
+	createMenu: function() {
+		Crafty.scene("menu", function(){
+			$("#menu").show();
+			// TODO: take player # input
+			var select = document.getElementById('playerNum');
+
+			// We need to remove all the options when the options get updated and recreate the select
+
+			// Create the select
+			for(var i = 0; i<gameOptions.MAX_PLAYERS; i++) {
+				select.options[select.options.length] = new Option(i+1, i+1);
+			}
+			$("#maxPlayers").html("Max Players: " + gameOptions.MAX_PLAYERS);
+
+			// TODO
+			// player name entry
+			// player character select: dropdown
+			// player color select: dropdown of colors
+		});
+	},
+
+	start: function() {
+		Game.createMenu();
+		Crafty.init(2216, 2216, document.getElementById('game')).canvas.init();
+		// Game pieces for the 4 pokemon you can potentially have
+		// Need to create background colors per user for these to sit on (MAYBE?)
+		/*Crafty.sprite("./gamepieces_alpha.png", {
+											bulbasaur:[7,7,50,50],
+											charmander:[398,10,50,50],
+											squirtle:[135,75,50,50],
+											pikachu:[523,265,50,50]
+										});*/
+
+
+
+		//Test Sprite animation
+		//Tiles are 65x65
+		var assetsObj = {
+    "sprites": {
+        "resources/gamepieces_alpha.png": {
+            tile: 65,
+            tileh: 65,
+            // We give names to three individual images
+            map: {
+                bulbasaur: [0, 0],
+								charmander:[6,0],
+								squirtle:[2,1],
+								pikachu:[8,4]
+
+            }
+        }
+    }
+};
+
+	  Crafty.load(assetsObj);
+		Crafty.enterScene("menu");
+	},
+
+	initPlayers: function(numPlayers, players) {
+		//TODO figure out how to resize image and zoom in on players
+		Crafty.scene("game", function() {
+			//Crafty.background("url('./wall-old.png')");
+			$("#menu").hide();
+			Crafty.e("2D, Canvas, Image").image("resources/wall-old.png");
+			currentNumPlayers = numPlayers;
+			for(i = 0;i < numPlayers;i++) {
+				// Create players, move to Pallet Town
+				playersArray[i] = Crafty.e("2D, Canvas,"+players[i].starter+",SpriteAnimation,Tween").reel("idle",1000,[[0,0],[1,0],[2,0],[3,0]])
+										.attr({x:325, y:1825}).animate("idle", -1);
+				playersArray[i].name = players[i].name;
+				// Set the players initial space on the board to pallet town
+				playersArray[i].space = 0;
+				// Keep the players initial status effect
+				playersArray[i].effect = "none";
+				// Idk what circle does
+				//Crafty.circle(0,0,10);
+			}
+
+		});
+
+		Game.playGame();
+		// Play the game after the players are created
+	},
+
+	playGame: function() {
+		Crafty.enterScene("game");
+		Crafty.viewport.clampToEntities = false;
+		Crafty.viewport.follow(playersArray[currentPlayer],-800,-800);
+		//Crafty.viewport.centerOn(playersArray[0], 3000);
+		//  On key press roll the dice
+		// Temporary code, testing stuff.
+		// On site press "R", look at top left of screen
+		Crafty.e("2D, Canvas, Color")
+			  .bind("KeyDown", function(e) {
+			  	if(e.key ==  Crafty.keys.R) {
+			  		player = playersArray[currentPlayer];
+			  		diceNum = rollDice(player);
+			  		moveSpaces(diceNum, player);
+			  		// TODO: move players
+			  		//       do space
+			  		//       move this code
+			  		$("#diceBlock").html(diceNum);
+			  		$("#playerSpace").html(player.name + ": " + player.space);
+
+
+			  		// This will probably be better if we define every space (with x,y coords) on the board since we'll need to
+			  		// display the rules for each square/what each square does
+
+
+			  		//currentY = player.y;
+			  		//currentX = player.x;
+			  		// Need to find determine how to move in a smaller rectangle each time
+			  		//if (y < )
+			  		//newY = currentY - (190*diceNum);
+			  		//player.attr({x:325, y:newY});
+			  		console.log(player);
+			  		// Go to the next player
+			  		nextPlayer(player);
+			  	}
+			  });
+	}
+};
---
+++ src/js/game/utils.js
@@ -0,0 +1,139 @@
+// Page utils
+// Form handlers etc.
+
+
+// Select number of players
+// TODO - add/remove inputs depending on selected players
+function generateMenu(numPlayers) {
+	// Generate the form:
+	for(i=0;i<numPlayers;i++) {
+	    $('#formTable').append(
+	        '<tr>'+
+	            '<td><input type="text" class="pname" id="player_'+i+'" name="player_'+i+'"></input></td>'+
+	            '<td><select class="pstarter" id="starter_'+i+'" name="starter_'+i+'">'+
+	                '<option></option>'+
+	                '<option value="squirtle">Squirtle</option>'+
+	                '<option value="charmander">Charmander</option>'+
+	                '<option value="bulbasaur">Bulbasaur</option>'+
+	            '</select></td>'+
+	        '</tr>'
+	    );
+	}
+}
+
+function updateOptions(maxPlayers) {
+	// TODO handle non-numerics
+	//		add more options
+	gameOptions.MAX_PLAYERS = maxPlayers;
+	$("#maxPlayers").html("Max Players: " + gameOptions.MAX_PLAYERS);
+	Crafty.enterScene("menu");
+}
+
+function rollDice(player)
+{	// Do we need player params or what space the player is on?
+	// Also in special cases where players move half spaces/double spaces we need to do something else
+	diceVal = Crafty.math.randomInt(1,6);
+	if (player.effect == "slow") {
+		// do we want to use floor or ceil?  hmmmmm
+		diceVal = Math.ceil(diceVal / 2);
+		// Remove effect
+		player.effect = "none";
+	}
+	if (player.effect == "fast") {
+		diceVal = diceVal * 2;
+		// Remove effect
+		player.effect = "none";
+	}
+	// Need to create an if statement if we need to stay on the same space but roll dice
+	return 1;
+}
+
+function pokemonFight(p1, p2) {
+	// if 1 pokemon is stronger than the other we need to roll 2 dice
+	// TODO
+}
+
+// Move the player, need to tweak x/y values a bit
+function moveSpaces(numberOfSpaces, player) {
+	oldSpace = player.space;
+	newSpace = player.space + numberOfSpaces;
+
+	newSpace = handleSpecialTiles(oldSpace, newSpace, player)
+
+	// Update the player space
+	player.space = newSpace;
+	moveTo = spaces[newSpace];
+
+//leverage tween for movement
+	player.tween({x: moveTo.x, y: moveTo.y}, 1000);
+}
+
+
+// This is the last thing that will happen at the end of a players turn
+function nextPlayer(player) {
+	// handle players with extra turns
+	if(player.effect == "extraTurn") {
+		// Remove the effect
+		player.effect = "none";
+		// Stay on the current player, they get another turn
+		return;
+	}
+
+	// since the current player starts at 0, we want to subtract 1
+	// so the current number reflects actual current player values
+	actualCurrentPlayers = currentNumPlayers-1;
+	if (currentPlayer >= actualCurrentPlayers) {
+		// Go back to player 1
+		currentPlayer = 0;
+	} else {
+		// Increment the player
+		currentPlayer++;
+	}
+}
+
+// Pokegyms we need the player to always stop at.
+// If player square >= required square
+// 		playerSq = rSq;
+//      do required square logic
+// else playerSq = square+diceroll
+// combine this with the spaces array?
+
+function handleSpecialTiles(oldSpace, newSpace, player) {
+	// Player wins, don't move call win command
+	if (newSpace > 71) {
+		return 71;
+		// TODO return win screen/animation
+	}
+
+	// First Abra teleport logic
+	if(newSpace == 11 && oldSpace < 11) {
+		return 28;
+	}
+	// Second Abra teleport logic
+	if(newSpace == 28 && (oldSpace > 11 && oldSpace < 28)) {
+		return 11;
+	}
+
+	if(newSpace == 2 || newSpace == 41) {
+		player.effect = "extraTurn";
+	}
+
+	if(newSpace == 3) {
+		// TODO give all other players the slow effect
+	}
+
+	if(newSpace == 20) {
+		player.effect = "fast";
+	}
+
+	if(newSpace == 4) {
+		// TODO
+		// update the player sprite
+		// we shouldn't have to create a new entity here?
+		player.sprite("pikachu");
+		player.starter = "pikachu";
+	}
+
+	// TODO add more special tiles
+	return newSpace;
+}
